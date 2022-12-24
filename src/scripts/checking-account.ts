45d35be53225fa96b56a924b2b1d3e3e0e2b8648
talbidev <talbidev@gmail.com>
2022-12-24 11:32:50 +0100

    first commit

---
+++ src/scripts/checking-account.ts
@@ -0,0 +1,23 @@
+﻿export class CheckingAccount {
+  private _balance = 0;//field
+
+  constructor(public title: string) { }
+
+  get balance(): number {
+    return this._balance;
+  }
+
+  set balance(v: number) {
+    if (v > 0) {
+      this._balance = v;
+    }
+  }
+
+  deposit(amount: number) {
+    this.balance += amount;
+  }
+
+  withdrawal(amount: number) {
+    this.balance -= amount;
+  }
+}
